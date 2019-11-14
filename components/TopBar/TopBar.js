40f028f812793b8ee642bcc27274725767b01854
Jess <jesszeroual@gmail.com>
2019-11-14 15:00:39 +0100

    Custom topBar

--- components/TopBar/TopBar.js
+++ components/TopBar/TopBar.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { NavigationContext } from 'react-navigation';
 
-import { TopBarWrapper, StyledIcon } from './TopBar.styled';
+import { TopBarWrapper, StyledIcon, Text, Span } from './TopBar.styled';
 
 const TopBar = () => {
   const navigation = useContext(NavigationContext);
@@ -11,6 +11,12 @@ const TopBar = () => {
       <TouchableOpacity onPress={() => navigation.openDrawer()}>
         <StyledIcon name="bars" />
       </TouchableOpacity>
+      {/* If there are props display => */}
+      <Text>
+        <Span>#</Span>
+        props.channel.name
+      </Text>
+      {/* Else display BrandLogo => */}
     </TopBarWrapper>
   );
 };
--- components/TopBar/TopBar.styled.js
+++ components/TopBar/TopBar.styled.js
@@ -3,16 +3,28 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 // eslint-disable-next-line import/prefer-default-export
 export const TopBarWrapper = styled.View`
-  height: 65px;
+  height: 70px;
   flex-direction: row;
-  justify-content: flex-end;
+  justify-content: flex-start;
   align-items: flex-end;
   border-bottom-width: 0.5px;
   border-bottom-color: #787878;
 `;
 
 export const StyledIcon = styled(Icon)`
-  color: #787878;
+  color: #282828;
   font-size: 25px;
   padding: 10px;
+`;
+
+export const Text = styled.Text`
+  color: #181818;
+  font-size: 18px;
+  font-weight: bold;
+  padding: 10px;
+`;
+
+export const Span = styled.Text`
+  display: flex;
+  color: #383838;
 `;
